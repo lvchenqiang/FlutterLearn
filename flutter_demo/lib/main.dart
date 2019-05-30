214e296c6e2fbb51e3cbb61a9d61b82f52d755b4
吕陈强 <lvchenqiang@gmail.com>
2019-05-30 16:31:30 +0800

    add  text

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp("Hello World"));
+
+import 'package:flutter_demo1/page/TextPage.dart';
+
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
-
-  String content;
-
-  MyApp(this.content);
 
   @override
   State<StatefulWidget> createState() {
@@ -18,13 +18,7 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
 
-  bool isShowText =true;
-
-  void increment(){
-    setState(() {
-      widget.content += "d";
-    });
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +27,52 @@ class MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-          appBar: AppBar(title: Text("Widget -- StatefulWidget及State"),),
-          body: Center(
-              child: GestureDetector(
-                child: isShowText? Text(widget.content) :null,
-                onTap: increment,
-              )
-          ),
-        )
+        home: MyHomePage(title: 'Flutter Widget Demo'),
     );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+final String title;
+
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+    List<String> widgets = [
+    '第14节 -- 文本框',
+    '第15节 -- 图片和Icon',
+    '第16节 -- 输入框',
+    '第17节 -- SnackBar',
+    '第18节 -- 对话框',
+    '第19节 -- BottomSheet',
+    '第20节 -- 菜单栏',
+    '第21节 -- 手势识别Widget',
+    '第24节 -- 弹性布局',
+    '第25节 -- 线性布局',
+    '第26节 -- 流式布局',
+    '第27节 -- 层叠布局',
+    '第28节 -- 容器类Widget',
+    '第29节 -- 功能类Widget',
+    '第30节 -- SingleChildScrollView',
+    '第31节 -- ListView',
+    '第32节 -- CustomScrollView',
+    '第33节 -- GridView',
+    '第34节 -- PageView',
+    '第52节 -- 响应式编程',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    )
   }
 }
---
+++ flutter_demo/lib/page/TextPage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo1/text/DefaultTextStyleWidget.dart';
+import 'package:flutter_demo1/text/RichTextWidget.dart';
+import 'package:flutter_demo1/text/TextWidget.dart';
+
+
+class TextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('文本框'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          GestureDetector(
+            child: ListTile(
+              title: Text('Text'),
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => TextWidget()));
+            },
+          ),
+          GestureDetector(
+            child: ListTile(
+              title: Text('RichText'),
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => RichTextWidget()));
+            },
+          ),
+          GestureDetector(
+            child: ListTile(
+              title: Text('DefaultTextStyle'),
+            ),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => DefaultTextStyleWidget()));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
