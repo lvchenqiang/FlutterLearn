60add534b1ab08a89db362fdd49e0340091bac5b
吕陈强 <lvchenqiang@gmail.com>
2019-06-02 23:18:46 +0800

    add base

---
+++ flutter_trip/lib/pages/travel_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TravelPage extends StatefulWidget {
+  TravelPage({Key key}) : super(key: key);
+
+  _TravelPageState createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+     child: Text("旅行"),
+    );
+  }
+}
