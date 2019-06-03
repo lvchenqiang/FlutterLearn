2875f30e2b9b4ff9e2e349096df1f47f790bfaae
吕陈强 <lvchenqiang@gmail.com>
2019-06-03 22:56:59 +0800

    add api

---
+++ flutter_trip/lib/model/home_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeModel{
+final ConfigModel model;
+
+  HomeModel(this.model);
+
+
+}
+
+
+
+class ConfigModel{
+final String searchUrl;
+
+ConfigModel({this.searchUrl});
+}
+
+
+class CommonMode {
+  
+}
