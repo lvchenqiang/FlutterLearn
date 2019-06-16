4cd1ef53597e9f86786715cd7a32e254993f2f71
吕陈强 <lvchenqiang@gmail.com>
2019-06-16 23:10:52 +0800

    add android plugin

--- flutter_trip/android/app/src/main/java/com/example/flutter_trip/MainActivity.java
+++ flutter_trip/android/app/src/main/java/com/example/flutter_trip/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.flutter_trip;
 
 import android.os.Bundle;
+
+import com.example.asr.plugin.AsrPlugin;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +12,11 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    registerSelfPlugin();
+  }
+
+  private void registerSelfPlugin() {
+    AsrPlugin.registerWith(registrarFor("com.example.asr.plugin.AsrPlugin"));
   }
 }
