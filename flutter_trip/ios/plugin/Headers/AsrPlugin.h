1a0a7da1dc3ec2f374eba4928d601aa871afe43a
吕陈强 <lvchenqiang@gmail.com>
2019-06-17 16:39:23 +0800

    add ios plugin

---
+++ flutter_trip/ios/plugin/Headers/AsrPlugin.h
@@ -0,0 +1,17 @@
+//
+//  AsrPlugin.h
+//  Runner
+//
+//  Created by 吕陈强 on 2019/6/17.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AsrPlugin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flutter_trip/ios/plugin/Headers/AsrPlugin.m
@@ -0,0 +1,13 @@
+//
+//  AsrPlugin.m
+//  Runner
+//
+//  Created by 吕陈强 on 2019/6/17.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import "AsrPlugin.h"
+
+@implementation AsrPlugin
+
+@end
