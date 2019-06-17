c0eb97d2ceae51cc240419bd7b1dab4497cbaf14
吕陈强 <lvchenqiang@gmail.com>
2019-06-17 16:22:07 +0800

    add ios config

---
+++ flutter_trip/ios/plugin/AsrManager.h
@@ -0,0 +1,17 @@
+//
+//  AsrManager.h
+//  Runner
+//
+//  Created by 吕陈强 on 2019/6/17.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AsrManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flutter_trip/ios/plugin/AsrManager.m
@@ -0,0 +1,13 @@
+//
+//  AsrManager.m
+//  Runner
+//
+//  Created by 吕陈强 on 2019/6/17.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import "AsrManager.h"
+
+@implementation AsrManager
+
+@end
