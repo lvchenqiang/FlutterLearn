33917b39ffacd7f5952001547f0eba9921e21374
吕陈强 <lvchenqiang@gmail.com>
2019-06-19 22:27:31 +0800

    add android

--- flutter_trip/android/app/src/main/java/com/example/flutter_trip/MainActivity.java
+++ flutter_trip/android/app/src/main/java/com/example/flutter_trip/MainActivity.java
@@ -7,9 +7,12 @@ import com.example.asr.plugin.AsrPlugin;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import org.devio.flutter.splashscreen.flutter_splash_screen.SplashScreen;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);// here
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
 
